abb791fc1a3c523add01c78755734f9852277711
thersvg <rodrigohend@outlook.com>
2025-03-31 19:05:47 -0300

    update

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -655,7 +655,7 @@ export function Customers() {
                     />
                   </div>
 
-                  <div>
+{/*                   <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">Inscrição Estadual</label>
                     <input
                       type="text"
@@ -663,7 +663,7 @@ export function Customers() {
                       onChange={(e) => setNewCustomer({ ...newCustomer, inscricaoEstadual: e.target.value })}
                       className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                     />
-                  </div>
+                  </div> */}
                   <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">E-mail</label>
                     <input
