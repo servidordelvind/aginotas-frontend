0afe684694f117395b615084af53c5da8fa8f1a2
Lucas Cordeiro <Lucas_tb1315@hotmail.com>
2025-04-03 03:05:45 +0000

    Logo

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CheckCircle2, Receipt } from 'lucide-react';
-import nomelogodelvind from '../public/logonomelogo.png';
+import nomelogodelvind from '../public/nomelogodelvind.png';
 import logodelvind from '../public/logodelvind.png';
 
 import { api } from '../lib/api';
