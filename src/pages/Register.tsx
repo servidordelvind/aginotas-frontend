9048100563fcfc0910fbf7b3dea7b83033b64b9c
thersvg <rodrigohend@outlook.com>
2025-04-03 16:14:27 -0300

    update

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -277,7 +277,7 @@ export function Register() {
 
 
     return subscription ? (
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
         <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-md">
           <div className="flex flex-col items-center mb-8">
         <Receipt className="w-12 h-12 text-blue-600 mb-4" />
@@ -387,8 +387,8 @@ export function Register() {
             </button>
           </form>
         </div>
-      </div>
-    ) : (
+    </div>
+/*     ) : (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
       <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-md">
         <div className="flex flex-col items-center mb-8">
@@ -503,8 +503,8 @@ export function Register() {
           </button>
         </form>
       </div>
-    </div>
-  ) : (
+    </div> */
+    ) : (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
       <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-md">
         <div className="flex flex-col items-center mb-8">
@@ -527,7 +527,7 @@ export function Register() {
         )}
 
 
-<div>
+      <div>
             <label htmlFor="cnpj" className="block text-sm font-medium text-gray-700 mb-1">
               CNPJ {loadingCnpj && <Loader2 className="inline w-4 h-4 ml-2 animate-spin" />}
             </label>
@@ -546,25 +546,6 @@ export function Register() {
               <p className="mt-1 text-sm text-red-600">{cnpjError}</p>
             )}
           </div>
-{/*        <div>
-          <label htmlFor="cnpj" className="block text-sm font-medium text-gray-700 mb-1">
-            CNPJ {loadingCnpj && <Loader2 className="inline w-4 h-4 ml-2 animate-spin" />}
-          </label>
-          <input
-            id="cnpj"
-            type="text"
-            value={cnpj}
-            onChange={(e) => setCnpj(formatCnpj(e.target.value))}
-            className={`w-full px-3 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${cnpjError ? 'border-red-500' : 'border-gray-300'
-
-              }`}
-            required
-            placeholder="00.000.000/0000-00"
-          />
-          {cnpjError && (
-            <p className="mt-1 text-sm text-red-600">{cnpjError}</p>
-          )}
-        </div> */}
 
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
